53f06708820e360d81819d757a9e3c225ba38c0c
“sx1x1” <1105158282@qq.com>
2021-09-19 22:28:15 +0800

    分词

---
+++ entry-analysis/src/main/java/person/sxr/entry/analysis/EntryAnalysisApplication.java
@@ -0,0 +1,13 @@
+package person.sxr.entry.analysis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EntryAnalysisApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EntryAnalysisApplication.class, args);
+    }
+
+}
---
+++ entry-analysis/src/test/java/person/sxr/entry/analysis/EntryAnalysisApplicationTests.java
@@ -0,0 +1,13 @@
+package person.sxr.entry.analysis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EntryAnalysisApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
