949e3e62701f201d962355bae198fb381c471537
“sx1x1” <1105158282@qq.com>
2021-10-05 10:05:22 +0800

    动图

---
+++ byte-dance/src/main/java/person/sxr/dance/ByteDanceApplication.java
@@ -0,0 +1,13 @@
+package person.sxr.dance;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ByteDanceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ByteDanceApplication.class, args);
+    }
+
+}
---
+++ byte-dance/src/test/java/person/sxr/dance/ByteDanceApplicationTests.java
@@ -0,0 +1,13 @@
+package person.sxr.dance;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ByteDanceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
